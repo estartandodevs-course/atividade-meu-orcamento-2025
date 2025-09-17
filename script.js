42c27dfeaeaab5531e72496dbf00c4ff52201be2
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2025-09-17 16:23:08 -0300

    feat: Adiciona calculo e exibição de resumo financeiro

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let transacoes = []
+  let transacoes = [];
+
+  carregarTransacoes();
+  atualizarResumo();
 
   function carregarTransacoes() {
     try {
-      const dados = localStorage.getItem('transacoes');
+      const dados = localStorage.getItem("transacoes");
       if (dados) {
         transacoes = JSON.parse(dados);
       } else {
@@ -13,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
       console.error("Erro ao carregar transações: ", error);
       transacoes = [];
     }
-  };
+  }
 
   function salvarTransacoes() {
     try {
@@ -21,34 +24,60 @@ document.addEventListener("DOMContentLoaded", () => {
     } catch (error) {
       console.error("Erro ao salvar transações: ", error);
     }
+  }
+
+  const formulario = document
+    .getElementById("form-transacao")
+    .addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      const inputDescricao = document.getElementById("input-descricao");
+      let descricao = inputDescricao.value;
+      const inputValor = document.getElementById("input-valor");
+      let valor = Number(inputValor.value);
+
+      if (!descricao.trim() || !valor) {
+        alert("Por favor preencha os campos com valor válido.");
+        return;
+      }
+
+      let transacao = {
+        id: new Date().getTime(),
+        descricao: descricao,
+        valor: valor,
+      };
+
+      transacoes.push(transacao);
+
+      salvarTransacoes();
+      inputDescricao.value = "";
+      inputValor.value = "";
+    });
+
+  function atualizarResumo() {
+    let somaReceita = transacoes
+      .filter((obj) => obj.valor)
+      .reduce((total, obj) => {
+        return obj.valor > 0 ? total + obj.valor : total;
+      }, 0);
+
+    let somaDespesas = transacoes
+      .filter((obj) => obj.valor)
+      .reduce((total, obj) => {
+        return obj.valor < 0 ? total + obj.valor : total;
+      }, 0);
+
+    let saldoFinal = somaReceita + somaDespesas;
+
+    document.getElementById("valor-receitas").textContent = formatarMoeda(somaReceita);
+    document.getElementById("valor-despesas").textContent = formatarMoeda(somaDespesas);
+    document.getElementById("valor-saldo").textContent = formatarMoeda(saldoFinal);
   };
 
-  const formulario = document.getElementById('form-transacao')
-  .addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const inputDescricao = document.getElementById('input-descricao');
-    const descricao = inputDescricao.value;
-    const inputValor = document.getElementById('input-valor');
-    const valor = Number(inputValor.value);
-    
-    if (!descricao.trim() || !valor) {
-      alert('Por favor preencha os campos com valor válido.');
-      return;
-    }
-
-    let transacao = {
-      id: new Date().getTime(),
-      descricao: descricao,
-      valor: valor,
-    };
-
-    transacoes.push(transacao);
-
-    salvarTransacoes();
-    inputDescricao.value= '';
-    inputValor.value = '';
-  });
-  
-  
+  function formatarMoeda(valor) {
+    return valor.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  }
 });
