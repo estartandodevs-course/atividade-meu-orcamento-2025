d1ac7d658603dfbf462554b1919c3d18190debde
Bruno Fabbio <brunobiodev@gmail.com>
2025-09-16 23:27:01 -0300

    feat: adiciona e organiza as variáveis

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 let transacoes = [];
+
+const form = document.getElementById("form-transacao");
+const inputDescricao = document.getElementById("input-descricao");
+const inputValor = document.getElementById("input-valor");
+const lista = document.getElementById("lista-transacoes");
+const spanReceitas = document.getElementById("valor-receitas");
+const spanDespesas = document.getElementById("valor-despesas");
+const spanSaldo = document.getElementById("valor-saldo");
