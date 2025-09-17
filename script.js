b8620e9dc904baaa93bc5afbb834308a44137692
Bruno Fabbio <brunobiodev@gmail.com>
2025-09-16 23:28:59 -0300

    feat: adiciona a função principal para o localStorage

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ const lista = document.getElementById("lista-transacoes");
 const spanReceitas = document.getElementById("valor-receitas");
 const spanDespesas = document.getElementById("valor-despesas");
 const spanSaldo = document.getElementById("valor-saldo");
+
+function carregarTransacoes() {
+  try {
+    const dados = localStorage.getItem("transacoes");
+    return dados ? JSON.parse(dados) : [];
+  } catch (e) {
+    console.error("Erro ao carregar do localStorage:", e);
+    return [];
+  }
+}
+
+function salvarTransacoes() {
+  localStorage.setItem("transacoes", JSON.stringify(transacoes));
+}
